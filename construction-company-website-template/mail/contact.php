0fbf83af9609e891680bb88e0bfeb34ceeae57c1
Jamiu2005 <olamidejamiu181@gmail.com>
2024-03-22 04:24:19 -0700

    My Name

---
+++ construction-company-website-template/mail/contact.php
@@ -0,0 +1,14 @@
+<!-- ?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message'])); -->
+
+<!-- $to = "info@example.com"; // Change this email to your // -->
+/
+?>
